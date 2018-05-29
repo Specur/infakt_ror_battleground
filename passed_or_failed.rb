784955c19ffa5b246ef55d468f25ae593ca04c35
Specur <mmmstec@o2.pl>
2018-05-29 21:04:25 +0200

    passed_or_failed_solution

--- passed_or_failed.rb
+++ passed_or_failed.rb
@@ -1,3 +1,20 @@
 class PassedOrFailed
 
+    def initialize(hash, bound)
+        @hash = hash
+        @bound = bound.to_i
+    end
+
+    def perform
+        failed = {}
+        passed = {}
+        @hash.each do |key, value|
+            if (value.to_i < @bound)
+                failed[key] = value.to_i
+            else
+                passed[key] = value.to_i
+            end
+        end
+        {passed: passed, failed: failed}
+    end
 end
